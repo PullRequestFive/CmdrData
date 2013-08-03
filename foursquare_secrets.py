0e82425b057b9af56c7b781ff87f18d96a41799c
Nat Welch <nat@natwelch.com>
2013-08-03 13:34:52 -0700

    whee

---
+++ foursquare_secrets.py
@@ -0,0 +1,6 @@
+SECRETS = {
+  # OAuth client secret. Must match what you set at https://foursquare.com/oauth.
+  'client_secret': 'SJCBU1RB5S1ZHFHYE4LFRHDMEJSZKUH1KAPBMSVHMGLRMHCO',
+  # Foursquare push API secret. Must match what you set at https://foursquare.com/oauth.
+  'push_secret': 'WUROAOSYSJ2M5ET5A32UPXBHV4WCBB4ULA2AYNW21NTGLLKV',
+}
