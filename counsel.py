636f6728c6244bc4dc9dad7bbce050b0182cc2f4
Kayvon Tehranian <ktehranian@gmail.com>
2013-08-03 13:51:00 -0700

    Progress on counsel

--- counsel.py
+++ counsel.py
@@ -2,11 +2,17 @@ import webapp2
 import json
 import urllib2
 import datetime
+import time
 
 class GenerateCounsel(webapp2.RequestHandler):
   def get(self):
     response = self.request.get('checkin')
     json_data = json.loads(response.text)
     user_id = json_data['user']['id']
-    parameters = 'limit=100&afterTimestamp='
+    now = datetime.datetime.now()
+    tsd = datetime.timedelta(days=7)
+    t = now - tsd
+    epoch_seconds = time.mktime(t.timetuple())
+    limit = 100
+    parameters = 'limit=%s&afterTimestamp=%s' % (limit, epoch_seconds)
     urllib2.urlopen('https://api.foursquare.com/v2/users/%s/checkins?' + parameter % user_id)
